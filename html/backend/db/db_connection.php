897515ca828d679122db61c57b5f4595129dd28f
Justin Chang <ycchang0324@gmail.com>
2020-08-14 23:39:29 +0800

    hi

--- html/backend/db/db_connection.php
+++ html/backend/db/db_connection.php
@@ -2,12 +2,13 @@
 
 function connection(){
     $servername = "db";
+    mysql_query("SET NAMES 'utf8'");
     $username = "root";
     $password = "root";
     $dbname = "second_hand_book";
 
     $conn = new mysqli($servername,$username,$password,$dbname);
-    mysql_query("SET NAMES 'utf8'");
+    
     return $conn;
 }
 ?>
