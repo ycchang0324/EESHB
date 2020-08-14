fb425e169597a06770e3017ebf678711441638ce
Justin Chang <ycchang0324@gmail.com>
2020-08-15 00:50:29 +0800

    try

--- html/backend/backEndSeller.php
+++ html/backend/backEndSeller.php
@@ -40,9 +40,14 @@ $others   = trimmedData( $data -> others );
 $stdId = strtolower($stdId);
 //創造一個$seller的物件
 
+
+$name = "張原嘉";
+$stdId = "b08901049";
 $category = "大一必修";
 $subject = "微積分";
-
+$price = 200;
+$fee = 100;
+$others = "嗨";
 $seller = new Seller($name, $stdId, $category, $subject,$price, 200 ,$others);
 
 //呼叫$seller的成員函式store()，將資料傳入資料庫中
