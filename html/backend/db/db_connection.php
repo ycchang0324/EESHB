fded16519c8279a5674b2ba281e6457f6a9fda5b
Justin Chang <ycchang0324@gmail.com>
2020-08-15 00:00:04 +0800

    conn

--- html/backend/db/db_connection.php
+++ html/backend/db/db_connection.php
@@ -7,7 +7,14 @@ function connection(){
     $dbname = "second_hand_book";
 
     $conn = new mysqli($servername,$username,$password,$dbname);
-    
+    mysqli_set_charset($conn, "utf8");//設定編碼為utf-8
+    mysql_query("SET names 'utf8'");
+    mysql_query("set character set 'utf8'",$conn);
+    mysql_query("SET character_set_database='utf8'",$conn);
+    mysql_query("SET character_set_client='utf8'",$conn);
+    mysql_query("SET character_set_results='utf8'",$conn);
+    mysql_query("SET character_set_server='utf8'",$conn);
+    mysql_query("SET character_set_connection='utf8'",$conn);
     return $conn;
 }
 ?>
