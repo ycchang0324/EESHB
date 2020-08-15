115fd5dd36f312787d98e87076092d6d8d66fc05
Justin Chang <ycchang0324@gmail.com>
2020-08-15 11:48:20 +0800

    oldBook

--- html/backend/sellOldBook.php
+++ html/backend/sellOldBook.php
@@ -17,7 +17,7 @@ $data = json_decode(file_get_contents("php://input"));
 
 //將POST過來的變數assign進php中的$stdId變數
 $id = $data -> id;
-$id = 3;
+//$id = 3;
 
 $conn = connection();
 $sql = "UPDATE oldbook SET isSold = 1 WHERE id = '$id'";
