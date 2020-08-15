8caebaf82a303503669538963438eb042726568a
Justin Chang <ycchang0324@gmail.com>
2020-08-15 09:33:05 +0800

    conn

--- html/backend/db/oldBookData.php
+++ html/backend/db/oldBookData.php
@@ -3,17 +3,9 @@
 require 'db_connection.php';
 
 
-$servername = "db";
-$username = "root";
-$password = "root";
-
-
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+$conn = connection();
+
 $sql = "
 CREATE TABLE `oldbook` (
   `id` int(5) NOT NULL,
