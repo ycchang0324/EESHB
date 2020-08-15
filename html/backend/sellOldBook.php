87e1c7fe75d4cbb493f4ae0e18d0589ffde20f8e
Justin Chang <ycchang0324@gmail.com>
2020-08-15 11:40:55 +0800

    route

--- html/backend/sellOldBook.php
+++ html/backend/sellOldBook.php
@@ -8,7 +8,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 //連線資料庫
-require_once '../db/db_connection.php';
+require_once './db/db_connection.php';
 
 
 
