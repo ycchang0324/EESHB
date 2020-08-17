92124fe9b17197616ce850ade36337d67beb6ee3
eeshb123rd <ntueeshb@gmail.com>
2020-08-17 15:15:33 +0800

    fonts

---
+++ html/precache-manifest.4f7995461febf8191de781ce05ed6afb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5706447e0af073f361e9ca9bf9246bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00070d8938acee567a16",
+    "url": "/static/css/main.cdbd44cf.chunk.css"
+  },
+  {
+    "revision": "ba0208f247b32ea1f281",
+    "url": "/static/js/2.66cf26d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66cf26d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00070d8938acee567a16",
+    "url": "/static/js/main.b57afe3c.chunk.js"
+  },
+  {
+    "revision": "b13b9283f2e3dfeae976",
+    "url": "/static/js/runtime-main.17ea016e.js"
+  },
+  {
+    "revision": "8b34790154f7ba7b9d229207c4a2f356",
+    "url": "/static/media/NotoSansTC-Regular.8b347901.otf"
+  },
+  {
+    "revision": "dddf9afea181eddb48a9b2df4cb90220",
+    "url": "/static/media/logo.dddf9afe.png"
+  }
+]);
