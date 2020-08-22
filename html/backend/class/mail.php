6e16ee0100460c602dc12e7599dccab56320012e
Justin Chang <ycchang0324@gmail.com>
2020-08-23 00:52:41 +0800

    1s

--- html/backend/class/mail.php
+++ html/backend/class/mail.php
@@ -222,7 +222,7 @@ class Mailer
                  $this -> addBody( $body );
                  $this -> sendMail();
                  $this -> removeAllRecipient();
-                 sleep(3);
+                 sleep(1);
                 
             }
             json_encode(["success"=>1,"msg"=>"send mail successfully"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
@@ -287,7 +287,7 @@ class Mailer
                  $this -> addBody( $body );
                  $this -> sendMail();
                  $this -> removeAllRecipient();
-                 sleep(3);
+                 sleep(1);
                 
             }
             json_encode(["success"=>1,"msg"=>"send mail successfully"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
@@ -403,7 +403,7 @@ class Mailer
                  $this -> addBody( $body );
                  $this -> sendMail();
                  $this -> removeAllRecipient();
-                 sleep(3);
+                 sleep(1);
                 //echo $name . "\n";
                 
             }
