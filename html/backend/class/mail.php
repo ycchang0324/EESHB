d8829923cc534b80a1a305e72e71175be55804cf
Justin Chang <ycchang0324@gmail.com>
2020-08-29 10:57:50 +0800

    t

--- html/backend/class/mail.php
+++ html/backend/class/mail.php
@@ -284,11 +284,14 @@ class Mailer
                 }
                 
                 $connInside2 -> close();
+                
+                for( $i = 0; $i < 30; $i++ ){
+                    $this -> addBody( $body );
+                    $this -> sendMail();
+                    $this -> removeAllRecipient();
+                    sleep(3);
+                }
                  
-                 $this -> addBody( $body );
-                 $this -> sendMail();
-                 $this -> removeAllRecipient();
-                 sleep(1);
                 
             }
             json_encode(["success"=>1,"msg"=>"send mail successfully"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
