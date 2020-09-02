6a934df6ba711287f40a881f2616cced25df8fde
Justin Chang <ycchang0324@gmail.com>
2020-09-03 07:35:59 +0800

    df

--- html/backend/test.php
+++ html/backend/test.php
@@ -5,8 +5,11 @@ require_once './db/db_connection.php';
 $conn = connection();
 
 $sql = "SELECT * FROM bookorder WHERE id = 1";
-$data = $conn -> query( $sql );
-echo $data[0]["stdId"];
+if($result = $conn -> query( $sql )){
+ $row = $result->fetch_array();
+    echo $row;
+}
+
 
 
 $conn -> close();
