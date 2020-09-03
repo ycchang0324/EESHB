7d4f6be03d853ea7ae429eef6257c762ecb9de5a
noidname <tim983649@gmail.com>
2020-09-04 01:37:13 +0800

    fix double send

---
+++ html/precache-manifest.4e5160add78e340f4745fb612000bc7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9c6b4019f73bb9ddddef4c06152a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d79b83409c3ab835535b",
+    "url": "/static/css/2.8223a7aa.chunk.css"
+  },
+  {
+    "revision": "ca17745afd93659b3717",
+    "url": "/static/css/main.3c734e37.chunk.css"
+  },
+  {
+    "revision": "d79b83409c3ab835535b",
+    "url": "/static/js/2.1576ed9a.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.1576ed9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca17745afd93659b3717",
+    "url": "/static/js/main.edf32055.chunk.js"
+  },
+  {
+    "revision": "1713ef52fda3b2998b11",
+    "url": "/static/js/runtime-main.8c818c92.js"
+  },
+  {
+    "revision": "8b34790154f7ba7b9d229207c4a2f356",
+    "url": "/static/media/NotoSansTC-Regular.8b347901.otf"
+  },
+  {
+    "revision": "dddf9afea181eddb48a9b2df4cb90220",
+    "url": "/static/media/logo.dddf9afe.png"
+  }
+]);
