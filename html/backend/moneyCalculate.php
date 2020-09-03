53948cc29dadf4fdf70f8ff89cd6673b50232d09
Justin Chang <ycchang0324@gmail.com>
2020-09-04 01:23:52 +0800

    debug money calculate

--- html/backend/moneyCalculate.php
+++ html/backend/moneyCalculate.php
@@ -13,13 +13,13 @@ $sql = "SELECT * FROM trancation WHERE IO = 'I'";
 $result = $conn->query($sql);
 
 while( $row = $result->fetch_assoc() )
-    $money += $row["price"];
+    $money += $row["ammount"];
 
 $sql = "SELECT * FROM trancation WHERE IO = 'O'";
 $result = $conn->query($sql);
 
 while( $row = $result->fetch_assoc() )
-    $money -= $row["price"];
+    $money -= $row["ammount"];
 
 echo "總金額是 " . $money . " 元."; 
 $conn->close();
