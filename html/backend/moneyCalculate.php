7eb00dafb13e52ed6697232e0b9511e51446838b
Justin Chang <ycchang0324@gmail.com>
2020-09-04 00:58:42 +0800

    create money calculate

---
+++ html/backend/moneyCalculate.php
@@ -0,0 +1,22 @@
+<?php
+
+
+//連線資料庫
+require_once './db/db_connection.php';
+
+
+$conn = connection();
+$sql = "SELECT * FROM trancation ";
+$result = $conn->query($sql);
+$money = 0;
+while( $row = $result->fetch_assoc() ){
+    if( $row["IO"] == "I" )
+        $money += $row["price"];
+    if( $row["IO" == "O"] )
+        $money -= $row["price"];
+}
+
+echo "總金額是 " . $money . "元."; 
+$conn->close();
+
+?>
