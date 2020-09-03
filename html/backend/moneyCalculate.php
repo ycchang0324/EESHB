2fd8dbacad6d4c3dba89822407618e9127cf9e75
Justin Chang <ycchang0324@gmail.com>
2020-09-04 01:04:44 +0800

    debug moneyCalculate

--- html/backend/moneyCalculate.php
+++ html/backend/moneyCalculate.php
@@ -11,12 +11,13 @@ $result = $conn->query($sql);
 $money = 0;
 while( $row = $result->fetch_assoc() ){
     if( $row["IO"] == "I" )
-        $money += $row["price"];
-    if( $row["IO" == "O"] )
-        $money -= $row["price"];
+        $money += $row[price];
+    
+    if( $row["IO"] == "O" )
+        $money -= $row[price];
 }
 
-echo "總金額是 " . $money . "元."; 
+echo "總金額是 " . $money . " 元."; 
 $conn->close();
 
 ?>
