d591fba032f0923d04cfb3b4c81c665097f98508
Justin Chang <ycchang0324@gmail.com>
2020-09-05 14:10:37 +0800

    oldbook plus id

--- html/service-worker.js
+++ html/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8bdb5463646e9e9c35ea91e3eab8b54.js"
+  "/precache-manifest.83e7dcbf0e4c928db98709cb9f5b1287.js"
 );
 
 self.addEventListener('message', (event) => {
