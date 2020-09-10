3593870698dd01edfe6e3959e332d2331563adcc
Justin Chang <ycchang0324@gmail.com>
2020-09-10 22:01:45 +0800

    debug picture

--- html/backend/searchOldBook.php
+++ html/backend/searchOldBook.php
@@ -26,8 +26,8 @@ if ($result->num_rows > 0) {
         echo "是否有貨： 尚有貨<br>";
     else 
         echo "是否有貨： 已賣出<br>";
-      $href = "../old_book_picture/image_" . $row["picture"] . ".jpg";
-    echo '<a href= $href >書籍圖片</a><br>';
+      $href =  . $row["picture"] . ".jpg";
+    echo '<a href= "../old_book_picture/image_" + $row["picture"] + ".jpg" >書籍圖片</a><br>';
       echo "<br>";
   }
 } else {
