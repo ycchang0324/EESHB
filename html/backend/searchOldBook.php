4084e09e222898e5aa6b551b374551efc03e0758
Justin Chang <ycchang0324@gmail.com>
2020-09-10 20:44:08 +0800

    debug

--- html/backend/searchOldBook.php
+++ html/backend/searchOldBook.php
@@ -13,7 +13,7 @@ $bookName = $_POST["bookName"];
 //呼叫連線資料庫，$conn現在是操作資料庫的變數
 $conn = connection();
 
-$sql = "SELECT * FROM bookorder WHERE name = '%bookName%'";
+$sql = "SELECT * FROM bookorder WHERE name = '%$bookName%'";
 $result = $conn->query($sql);
 //if($result->num_rows > 0){
 $orderList = $result -> fetch_all(MYSQLI_ASSOC);
